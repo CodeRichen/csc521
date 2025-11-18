2a099a9e25f87c2bc319e4a5dcde96c1bcfd0127
A1125515 <A1125515@mail.nuk.edu.tw>
2025-11-18 14:59:08 +0800

    result2 succes

--- lab4/src/config.c
+++ lab4/src/config.c
@@ -18,7 +18,7 @@
     uint8_t defarpip[4]   = {0};
 uint8_t defpingip[4]  = {140, 127, 208, 18};  // 系網預設目標
 
-uint8_t defdnsip[4]   = {8, 8, 8, 8};         // Google DNS 伺服器
+uint8_t defdnsip[4]   = {1, 1, 1, 1};         // Cloudflare DNS 伺服器
 char *defdnsquery     = "csie.nuk.edu.tw";    // 資工系網域名
 uint16_t tcp_filter_port = 0x5515;            // 修正為學號末四碼
 
